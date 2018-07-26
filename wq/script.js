ae9b05f2c3a2b2ab0ffdb5b47176ad81c56dab1f
Andy Bell <BellAn@si.edu>
2018-07-26 16:43:11 -0400

    adding wq folder

---
+++ wq/script.js
@@ -0,0 +1,35 @@
+var tabulate = function (data,columns) {
+  var table = d3.select('body').append('table')
+	var thead = table.append('thead')
+	var tbody = table.append('tbody')
+
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('https://dl.dropboxusercontent.com/s/a3x2ppnw6kwzbz2/data.csv?dl=0',function (data) {
+	var columns = ['Temperature','SpecificConductivity','Conductivity']
+  console.log(data)
+  tabulate(data,columns)
+})
